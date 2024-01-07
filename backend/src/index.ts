b240b76f6913c7162ce79acc6843bdb1a2e8767e
Juan Sulca <juanm.sulca@gmail.com>
2024-01-06 19:58:52 -0500

    Database connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 dotenv.config();
 
@@ -8,7 +11,16 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
+
+  const newUser = await prisma.user.create({
+    data: {
+      firstname: 'Alice',
+      lastname: 'LASD',
+    },
+  })
+
+  const users = await prisma.user.findMany()
   res.send("Express + TypeScript Server");
 });
 
